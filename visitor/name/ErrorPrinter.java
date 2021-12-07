7d3ac475e7ca671a4f4d691fe789812df1fb4d6b
Javad <javad.zahedi2001@gmail.com>
2021-12-07 15:12:01 +0330

    some visitors ware added

--- visitor/name/ErrorPrinter.java
+++ visitor/name/ErrorPrinter.java
@@ -19,19 +19,20 @@ public class ErrorPrinter extends Visitor<Void> {
         System.out.println("Line " + line + ": " + message);
     }
     private int numberOfErrors;
-    public int getNumberOfErrors() { return this.numberOfErrors; }
-    public ErrorPrinter(){
+    public int getNumberOfErrors() { return numberOfErrors; }
+    public ErrorPrinter() {
         numberOfErrors = 0;
     }
+
     @Override
     public Void visit(Program program) {
         SymbolTable programST = new SymbolTable();
         SymbolTable.push(programST);
         SymbolTable.root = programST;
         for (StructDeclaration structDeclaration : program.getStructs()) {
-//            SymbolTable structSymbolTable = new SymbolTable(SymbolTable.top);
-//            SymbolTable.push(structSymbolTable);
-            //structDeclaration.accept(this);
+            // SymbolTable structSymbolTable = new SymbolTable(SymbolTable.top);
+            // SymbolTable.push(structSymbolTable);
+            // structDeclaration.accept(this);
             StructSymbolTableItem structSymbolTableItem = new StructSymbolTableItem(structDeclaration);
             structSymbolTableItem.setStructSymbolTable(SymbolTable.top);
             SymbolTable.pop();
@@ -46,11 +47,11 @@ public class ErrorPrinter extends Visitor<Void> {
             }
         }
         for (FunctionDeclaration functionDeclaration : program.getFunctions()) {
-//            SymbolTable functionSymbolTable = new SymbolTable(SymbolTable.top);
-//            SymbolTable.push(functionSymbolTable);
-            FunctionSymbolTableItem functionSymbolTableItem = new FunctionSymbolTableItem(functionDeclaration);
-//            functionSymbolTableItem.setFunctionSymbolTable(SymbolTable.top);
-//            SymbolTable.pop();
+            // SymbolTable functionSymbolTable = new SymbolTable(SymbolTable.top);
+            // SymbolTable.push(functionSymbolTable);
+            // FunctionSymbolTableItem functionSymbolTableItem = new FunctionSymbolTableItem(functionDeclaration);
+            // functionSymbolTableItem.setFunctionSymbolTable(SymbolTable.top);
+            // SymbolTable.pop();
             try {
                 programST.put(functionSymbolTableItem);//todo try catch
             }
@@ -60,11 +61,11 @@ public class ErrorPrinter extends Visitor<Void> {
                         functionDeclaration.getLine(), functionSymbolTableItem.getName());
                 System.out.println(duplicateFunction.getMessage());
             }
-            //functionDeclaration.accept(this);
+            functionDeclaration.accept(this);
         }
         MainDeclaration mainDeclaration = program.getMain();
-//        SymbolTable mainSymbolTable = new SymbolTable(SymbolTable.top);
-//        SymbolTable.push(mainSymbolTable);
+        SymbolTable mainSymbolTable = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(mainSymbolTable);
         mainDeclaration.accept(this);
         SymbolTable.pop();
         SymbolTable.pop(); // pop program
@@ -86,53 +87,51 @@ public class ErrorPrinter extends Visitor<Void> {
         SymbolTable mainSymbolTable = new SymbolTable(SymbolTable.top);
         SymbolTable.push(mainSymbolTable);
         mainDec.getBody().accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(VariableDeclaration variableDec) {
-        //todo
-        messagePrinter(variableDec.getLine(), variableDec.toString());
-        variableDec.getVarName().accept(this);
-        if (variableDec.getDefaultValue() != null)
-            variableDec.getDefaultValue().accept(this);
+        VariableSymbolTableItem variableSymbolTableItem;
+        variableSymbolTableItem = new VariableSymbolTableItem(variableDec.getVarName());
+        try {
+            SymbolTable.top.put(variableSymbolTableItem);
+        }
+        catch (ItemAlreadyExistsException ex) {
+            DuplicateVar duplicateVar;
+            duplicateVar = new DuplicateVar(variableDec.getLine(), variableSymbolTableItem.getName());
+            System.out.println(duplicateVar.getMessage());
+        }
         return null;
     }
 
     @Override
     public Void visit(StructDeclaration structDec) {
-        //todo
-        messagePrinter(structDec.getLine(), structDec.toString());
-        structDec.getStructName().accept(this);
+        SymbolTable structSymbolTable = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(structSymbolTable);
         structDec.getBody().accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(SetGetVarDeclaration setGetVarDec) {
-        //todo
-        messagePrinter(setGetVarDec.getLine(), setGetVarDec.toString());
-        setGetVarDec.getVarName().accept(this);
+        SymbolTable setGetSymbolTable = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(setGetSymbolTable);
         for (VariableDeclaration variableDeclaration : setGetVarDec.getArgs())
             variableDeclaration.accept(this);
-        setGetVarDec.getSetterBody().accept(this);
-        setGetVarDec.getGetterBody().accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(AssignmentStmt assignmentStmt) {
-        //todo
-        messagePrinter(assignmentStmt.getLine(), assignmentStmt.toString());
-        assignmentStmt.getLValue().accept(this);
-        assignmentStmt.getRValue().accept(this);
         return null;
     }
 
     @Override
     public Void visit(BlockStmt blockStmt) {
-        //todo
-        messagePrinter(blockStmt.getLine(), blockStmt.toString());
         for (Statement statement: blockStmt.getStatements())
             statement.accept(this);
         return null;
@@ -140,52 +139,46 @@ public class ErrorPrinter extends Visitor<Void> {
 
     @Override
     public Void visit(ConditionalStmt conditionalStmt) {
-        //todo
-        messagePrinter(conditionalStmt.getLine(), conditionalStmt.toString());
-        conditionalStmt.getCondition().accept(this);
+        SymbolTable thenBodySymbolTable = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(thenBodySymbolTable);
         conditionalStmt.getThenBody().accept(this);
-        if (conditionalStmt.getElseBody() != null)
+        SymbolTable.pop();
+
+        if (conditionalStmt.getElseBody() != null) {
+            SymbolTable elseBodySymbolTable = new SymbolTable(SymbolTable.top);
+            SymbolTable.push(elseBodySymbolTable);
             conditionalStmt.getElseBody().accept(this);
+            SymbolTable.pop();
+        }
         return null;
     }
 
     @Override
     public Void visit(FunctionCallStmt functionCallStmt) {
-        //todo
-        messagePrinter(functionCallStmt.getLine(), functionCallStmt.toString());
-        functionCallStmt.getFunctionCall().accept(this);
         return null;
     }
 
     @Override
     public Void visit(DisplayStmt displayStmt) {
-        //todo
-        messagePrinter(displayStmt.getLine(), displayStmt.toString());
-        displayStmt.getArg().accept(this);
         return null;
     }
 
     @Override
     public Void visit(ReturnStmt returnStmt) {
-        //todo
-        messagePrinter(returnStmt.getLine(), returnStmt.toString());
-        returnStmt.getReturnedExpr().accept(this);
         return null;
     }
 
     @Override
     public Void visit(LoopStmt loopStmt) {
-        //todo
-        messagePrinter(loopStmt.getLine(), loopStmt.toString());
-        loopStmt.getCondition().accept(this);
+        SymbolTable loopStmtSymbolTable = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(loopStmtSymbolTable);
         loopStmt.getBody().accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(VarDecStmt varDecStmt) {
-        //todo
-        messagePrinter(varDecStmt.getLine(), varDecStmt.toString());
         for (VariableDeclaration variableDeclaration: varDecStmt.getVars())
             variableDeclaration.accept(this);
         return null;
@@ -193,109 +186,103 @@ public class ErrorPrinter extends Visitor<Void> {
 
     @Override
     public Void visit(ListAppendStmt listAppendStmt) {
-        //todo
-        messagePrinter(listAppendStmt.getLine(), listAppendStmt.toString());
-        listAppendStmt.getListAppendExpr().accept(this);
         return null;
     }
 
     @Override
     public Void visit(ListSizeStmt listSizeStmt) {
-        //todo
-        messagePrinter(listSizeStmt.getLine(), listSizeStmt.toString());
-        listSizeStmt.getListSizeExpr().accept(this);
         return null;
     }
 
-    @Override
-    public Void visit(BinaryExpression binaryExpression) {
-        //todo
-        messagePrinter(binaryExpression.getLine(), binaryExpression.toString());
-        binaryExpression.getFirstOperand().accept(this);
-        binaryExpression.getSecondOperand().accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(BinaryExpression binaryExpression) {
+    //     //todo
+    //     messagePrinter(binaryExpression.getLine(), binaryExpression.toString());
+    //     binaryExpression.getFirstOperand().accept(this);
+    //     binaryExpression.getSecondOperand().accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(UnaryExpression unaryExpression) {
-        //todo
-        messagePrinter(unaryExpression.getLine(), unaryExpression.toString());
-        unaryExpression.getOperand().accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(UnaryExpression unaryExpression) {
+    //     //todo
+    //     messagePrinter(unaryExpression.getLine(), unaryExpression.toString());
+    //     unaryExpression.getOperand().accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(FunctionCall funcCall) {
-        //todo
-        messagePrinter(funcCall.getLine(), funcCall.toString());
-        funcCall.getInstance().accept(this);
-        for (Expression expression : funcCall.getArgs())
-            expression.accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(FunctionCall funcCall) {
+    //     //todo
+    //     messagePrinter(funcCall.getLine(), funcCall.toString());
+    //     funcCall.getInstance().accept(this);
+    //     for (Expression expression : funcCall.getArgs())
+    //         expression.accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(Identifier identifier) {
-        //todo
-        messagePrinter(identifier.getLine(), identifier.toString());
-        return null;
-    }
+    // @Override
+    // public Void visit(Identifier identifier) {
+    //     //todo
+    //     messagePrinter(identifier.getLine(), identifier.toString());
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(ListAccessByIndex listAccessByIndex) {
-        //todo
-        messagePrinter(listAccessByIndex.getLine(), listAccessByIndex.toString());
-        listAccessByIndex.getInstance().accept(this);
-        listAccessByIndex.getIndex().accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(ListAccessByIndex listAccessByIndex) {
+    //     //todo
+    //     messagePrinter(listAccessByIndex.getLine(), listAccessByIndex.toString());
+    //     listAccessByIndex.getInstance().accept(this);
+    //     listAccessByIndex.getIndex().accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(StructAccess structAccess) {
-        //todo
-        messagePrinter(structAccess.getLine(), structAccess.toString());
-        structAccess.getInstance().accept(this);
-        structAccess.getElement().accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(StructAccess structAccess) {
+    //     //todo
+    //     messagePrinter(structAccess.getLine(), structAccess.toString());
+    //     structAccess.getInstance().accept(this);
+    //     structAccess.getElement().accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(ListSize listSize) {
-        //todo
-        messagePrinter(listSize.getLine(), listSize.toString());
-        listSize.getArg().accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(ListSize listSize) {
+    //     //todo
+    //     messagePrinter(listSize.getLine(), listSize.toString());
+    //     listSize.getArg().accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(ListAppend listAppend) {
-        //todo
-        messagePrinter(listAppend.getLine(), listAppend.toString());
-        listAppend.getListArg().accept(this);
-        listAppend.getElementArg().accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(ListAppend listAppend) {
+    //     //todo
+    //     messagePrinter(listAppend.getLine(), listAppend.toString());
+    //     listAppend.getListArg().accept(this);
+    //     listAppend.getElementArg().accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(ExprInPar exprInPar) {
-        //todo
-        messagePrinter(exprInPar.getLine(), exprInPar.toString());
-        for (Expression expression : exprInPar.getInputs())
-            expression.accept(this);
-        return null;
-    }
+    // @Override
+    // public Void visit(ExprInPar exprInPar) {
+    //     //todo
+    //     messagePrinter(exprInPar.getLine(), exprInPar.toString());
+    //     for (Expression expression : exprInPar.getInputs())
+    //         expression.accept(this);
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(IntValue intValue) {
-        //todo
-        messagePrinter(intValue.getLine(), intValue.toString());
-        return null;
-    }
+    // @Override
+    // public Void visit(IntValue intValue) {
+    //     //todo
+    //     messagePrinter(intValue.getLine(), intValue.toString());
+    //     return null;
+    // }
 
-    @Override
-    public Void visit(BoolValue boolValue) {
-        //todo
-        messagePrinter(boolValue.getLine(), boolValue.toString());
-        return null;
-    }
+    // @Override
+    // public Void visit(BoolValue boolValue) {
+    //     //todo
+    //     messagePrinter(boolValue.getLine(), boolValue.toString());
+    //     return null;
+    // }
 }
